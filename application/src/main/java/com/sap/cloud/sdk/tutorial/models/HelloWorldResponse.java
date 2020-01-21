d812add9543ed35f10ce2d850bb1e34663315112
jyotichaudhury <jyotichaudhury@gmail.com>
2020-01-21 14:55:15 -0500

    initial commit

---
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/models/HelloWorldResponse.java
@@ -0,0 +1,13 @@
+package com.sap.cloud.sdk.tutorial.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class HelloWorldResponse
+{
+    @JsonProperty("hello")
+    private final String name;
+
+    public HelloWorldResponse( final String name ) {
+        this.name = name;
+    }
+}
